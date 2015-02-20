c9df1d75b2375185e61281cbad2beb9b6914f95b
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-20 10:35:23 -0500

    ...

--- load_fn.py
+++ load_fn.py
@@ -16,5 +16,4 @@ def load_data(typeData, labelsInfo, imageSize, path):
         nameFile = "{0}/{1}Resized/{2}.Bmp".format(path, typeData, idImage)
         img = imread(nameFile, as_grey=True)
         x[index, :] = np.reshape(img, (1, imageSize))
-    print x.shape
     return x
--- my_model.py
+++ my_model.py
@@ -8,9 +8,6 @@ import pylab as pl
 import pandas as pd
 
 from load_fn import load_data
-from lasagne import layers
-from lasagne.updates import nesterov_momentum
-from nolearn.lasagne import NeuralNet
 
 from sklearn.cross_validation import cross_val_score as k_fold_CV
 from sklearn.cross_validation import train_test_split
@@ -32,27 +29,32 @@ def transform_feature_to_str(ft):
 def float32(k):
     return np.cast['float32'](k)
 
-model = NeuralNet(
-    layers=[ # three layers: one hidden layer
-        ('input', layers.InputLayer),
-        ('hidden', layers.DenseLayer),
-        ('output', layers.DenseLayer),],
-    # layer parameters:
-    input_shape=(None, 400),  # 96x96 input pixels per batch
-    hidden_num_units=50,  # number of units in hidden layer
-    output_nonlinearity=None,  # output layer uses identity function
-    output_num_units=1,  # 30 target values
 
-    # optimization method:
-    update=nesterov_momentum,
-    update_learning_rate=0.01,
-    update_momentum=0.9,
+def train_nn_model():
+    from lasagne import layers
+    from lasagne.updates import nesterov_momentum
+    from nolearn.lasagne import NeuralNet
 
-    regression=True,  # flag to indicate we're dealing with regression problem
-    max_epochs=200,  # we want to train this many epochs
-    verbose=1,)
+    model = NeuralNet(
+        layers=[ # three layers: one hidden layer
+            ('input', layers.InputLayer),
+            ('hidden', layers.DenseLayer),
+            ('output', layers.DenseLayer),],
+        # layer parameters:
+        input_shape=(None, 400),  # 96x96 input pixels per batch
+        hidden_num_units=50,  # number of units in hidden layer
+        output_nonlinearity=None,  # output layer uses identity function
+        output_num_units=1,  # 30 target values
 
-def train_model():
+        # optimization method:
+        update=nesterov_momentum,
+        update_learning_rate=0.01,
+        update_momentum=0.9,
+
+        regression=True,  # flag to indicate we're dealing with regression problem
+        max_epochs=200,  # we want to train this many epochs
+        verbose=1,)
+
     imageSize = 400 # 20 x 20 pixels
 
     #Set location of data files , folders
@@ -82,6 +84,35 @@ def train_model():
     ytest_pred = model.predict(xtest)
     print model.accuracy_score(ytest_pred,ytest)
 
+def train_model():
+    imageSize = 400 # 20 x 20 pixels
+
+    #Set location of data files , folders
+    path = '.'
+
+    labelsInfoTrain = pd.read_csv("{0}/trainLabels.csv".format(path))
+
+    #Read training matrix
+    xTrain = load_data("train", labelsInfoTrain, imageSize, path)
+
+    #Read information about test data ( IDs ).
+    labelsInfoTest = pd.read_csv("{0}/sampleSubmission.csv".format(path))
+
+    #Read test matrix
+    xTest = load_data("test", labelsInfoTest, imageSize, path)
+
+    yTrain = labelsInfoTrain['Class'].map(transform_str_to_feature)
+
+    print yTrain
+
+    xtrain, xtest, ytrain, ytest = train_test_split(xTrain, yTrain, test_size=0.5)
+
+    model = RandomForestClassifier(n_estimators=200)
+    model.fit(xtrain, ytrain)
+    ytest_pred = model.predict(xtest)
+    print model.accuracy_score(ytest_pred,ytest)
+
+
 def get_submission():
     imageSize = 400 # 20 x 20 pixels
 
@@ -101,6 +132,7 @@ def get_submission():
 
     yTrain = labelsInfoTrain['Class'].map(transform_str_to_feature)
     
+    model = RandomForestClassifier(n_estimators=200)
     model.fit(xTrain, yTrain)
     yTest = model.predict(xTest)
     
