d753ccb6e9148c33c7700e1c69cd22c87df00c74
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-23 16:15:54 -0500

    ...

--- load_fn.py
+++ load_fn.py
@@ -15,7 +15,7 @@ def load_data(typeData, labelsInfo, imageSize, path):
     for (index, idImage) in enumerate(labelsInfo["ID"]):
         nameFile = "{0}/{1}Resized/{2}.Bmp".format(path, typeData, idImage)
         img = imread(nameFile, as_grey=True)
-        print img.shape
+        print img.shape, img.dtype
         exit(0)
         x[index, :] = np.reshape(img, (1, imageSize))
     return x
