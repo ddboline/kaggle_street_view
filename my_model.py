c00e1361e04d55381b6abdd223c56cb7a5dad3e1
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-23 16:24:38 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -78,12 +78,12 @@ def train_nn_model():
         output_num_units=1,  # 30 target values
 
         # optimization method:
-        update=nesterov_momentum,
-        update_learning_rate=0.01,
-        update_momentum=0.9,
+        #update=nesterov_momentum,
+        #update_learning_rate=0.01,
+        #update_momentum=0.9,
 
-        regression=True,  # flag to indicate we're dealing with regression problem
-        max_epochs=200,  # we want to train this many epochs
+        #regression=True,  # flag to indicate we're dealing with regression problem
+        #max_epochs=200,  # we want to train this many epochs
         verbose=0,)
 
     xTrain, yTrain, xTest, labelsInfoTest = load_train_test_data()
