b586034e831c4ebab3b48f44a4c6ad24a4a2e0f2
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-23 16:33:33 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -18,19 +18,20 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 
 ORD_VALUES = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
+NORD = len(ORD_VALUES)
 
 def transform_str_to_feature(st):
     #return ord(st)
     ordval = ord(st)
     ordidx = ORD_VALUES.index(ordval)
-    return (ordidx)/float(len(ORD_VALUES))
+    return (ordidx-NORD//2)/float(NORD//2)
 
 def transform_feature_to_str(ft):
     #return chr(ft)
-    idx = int(ft * len(ORD_VALUES))
+    idx = NORD//2 + int(ft * NORD//2)
     if idx < 0 :
         idx = 0
-    if idx >= len(ORD_VALUES):
+    if idx >= NORD:
         idx = ORD_VALUES-1
     ordval = ORD_VALUES[idx]
     return chr(ordval)
