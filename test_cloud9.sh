3692d171a5d03504e0d8fccddda9379ef650af3e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-23 14:35:39 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -3,6 +3,8 @@
 sudo modprobe nvidia-340
 sudo modprobe nvidia-340-uvm
 
+sudo apt-get install -y python-skimage
+
 export PATH="/usr/local/cuda-6.5/bin:$PATH"
 export LD_LIBRARY_PATH="/usr/local/cuda-6.5/lib64"
 
@@ -12,13 +14,14 @@ rm *.pickle *.png
 touch output.out
 touch output.err
 # ./test.py >> output.out 2>> output.err
-./my_model.py $1 >> output.out 2>> output.err
+# ./my_model.py $1 >> output.out 2>> output.err
+./my_model.py
 
-if [ -z $1 ]; then
-    D=`date +%Y%m%d%H%M%S`
-else
-    D=$1_`date +%Y%m%d%H%M%S`
-fi
+# if [ -z $1 ]; then
+#     D=`date +%Y%m%d%H%M%S`
+# else
+#     D=$1_`date +%Y%m%d%H%M%S`
+# fi
 # ssh ddboline@ddbolineathome.mooo.com "mkdir -p ~/setup_files/build/kaggle_facial_keypoints/output_${D}"
 # scp output.out output.err *.png *.pickle submission*.csv ddboline@ddbolineathome.mooo.com:~/setup_files/build/kaggle_facial_keypoints/output_${D}
 # ssh ddboline@ddbolineathome.mooo.com "~/bin/send_to_gtalk DONE_${D}"
