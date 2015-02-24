d7f1de6390a63fae4d61dc7e2364fb358bd2de6e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-24 08:30:26 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -53,6 +53,8 @@ def load_train_test_data():
 
     yTrain = labelsInfoTrain['Class'].map(transform_str_to_feature).astype(np.float32)
 
+    print yTrain
+
     #Read information about test data ( IDs ).
     labelsInfoTest = pd.read_csv("{0}/sampleSubmission.csv".format(path))
 
