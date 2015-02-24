1ae0ebc931aadc04386c6b4d430c96c9f7d11a2e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-24 08:32:28 -0500

    ...

--- my_model.py
+++ my_model.py
@@ -77,7 +77,7 @@ def train_nn_model():
             ('output', layers.DenseLayer),],
         # layer parameters:
         input_shape=(None, imageSize),  # 96x96 input pixels per batch
-        hidden_num_units=100,  # number of units in hidden layer
+        hidden_num_units=500,  # number of units in hidden layer
         output_nonlinearity=None,  # output layer uses identity function
         output_num_units=1,  # 30 target values
 
