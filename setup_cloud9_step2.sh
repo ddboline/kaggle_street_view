6dbbc3d81ee0c021bdd1e29f269f614f7d0b295e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-24 09:31:14 -0500

    ...

--- setup_cloud9_step2.sh
+++ setup_cloud9_step2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH="/usr/local/cuda-6.5/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-6.5/lib64"
+
 sudo apt-get remove -y linux-headers-3.13.0-36 linux-headers-3.13.0-36-generic linux-image-3.13.0-36-generic
 sudo apt-get install -y --reinstall nvidia-340 nvidia-340-dev nvidia-340-uvm
 
@@ -15,7 +18,7 @@ sudo modprobe nvidia-340-uvm
 ### getting this to work may require:
 ### installing from https://github.com/Theano/Theano.git
 ### changing a line in theano/sandbox/cuda/cuda_ndarray.cuh (more likely the former than the latter)
-sudo pip install --upgrade git+https://github.com/Theano/Theano.git 2>&1 > theano_build.log
+sudo pip install git+https://github.com/Theano/Theano.git 2>&1 > theano_build.log
 sudo pip install git+https://github.com/benanne/Lasagne.git 2>&1 > lasagne_build.log
 sudo pip install git+https://github.com/dnouri/nolearn.git 2>&1 > nolearn_build.log
 
